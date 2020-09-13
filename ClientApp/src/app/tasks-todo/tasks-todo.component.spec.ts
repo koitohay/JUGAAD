fea677c3922dbb1682586b3f42504399b9fc338f
Kaleem Ullah <kau@netcompany.com>
2020-09-13 03:03:31 +0200

    Added a new component

---
+++ ClientApp/src/app/tasks-todo/tasks-todo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TasksTodoComponent } from './tasks-todo.component';
+
+describe('TasksTodoComponent', () => {
+  let component: TasksTodoComponent;
+  let fixture: ComponentFixture<TasksTodoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TasksTodoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TasksTodoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
